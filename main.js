7ac9efbb9cb2d02142bed8df950ffd598100d61e
andersonmg89 <andersonmg89@gmail.com>
2021-11-22 19:36:55 -0300

    :tada: Create CardGame component

---
+++ main.js
@@ -0,0 +1,11 @@
+import CardGame from "./src/components/CardGame";
+import "./src/styles/settings/colors.css"
+import "./src/styles/generic/reset.css"
+import "./src/styles/elements/base.css"
+
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame();
+
+$root.insertAdjacentHTML("beforeend", CardGame())
+
+
